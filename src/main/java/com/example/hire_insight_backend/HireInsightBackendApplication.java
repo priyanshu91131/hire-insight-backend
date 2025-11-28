1859024c7dbcbb9b417107cedab61d3afbb3c4ed
priyanshu91131 <92662368+priyanshu91131@users.noreply.github.com>
2025-11-28 21:33:47 +0530

    Initial backend project

---
+++ src/main/java/com/example/hire_insight_backend/HireInsightBackendApplication.java
@@ -0,0 +1,15 @@
+package com.example.hire_insight_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HireInsightBackendApplication {
+
+	public static void main(String[] args) {
+
+		SpringApplication.run(HireInsightBackendApplication.class, args);
+
+	}
+
+}
---
+++ src/test/java/com/example/hire_insight_backend/HireInsightBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hire_insight_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HireInsightBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
